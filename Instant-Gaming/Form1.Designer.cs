67b3a32d799eb15b7e5205a3a1f9a17c28fdafc2
Jacko04 <Kosterschristian@gmail.com>
2021-11-23 08:55:21 +0100

    LOL Update

--- Instant-Gaming/Form1.Designer.cs
+++ Instant-Gaming/Form1.Designer.cs
@@ -34,11 +34,11 @@ namespace Instant_Gaming
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(384, 132);
+            this.button1.Location = new System.Drawing.Point(352, 292);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "LOL";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
