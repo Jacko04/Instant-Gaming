f96d7021b3f8cd926058cfdd63c627403eba3d7b
Spawnpoint31 <erikbe10@yahoo.de>
2021-11-26 11:53:54 +0100

    Verkaufsform erstellt

--- Instant-Gaming/Verkauf.Designer.cs
+++ Instant-Gaming/Verkauf.Designer.cs
@@ -28,50 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pn_Funktion = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // pn_Funktion
-            // 
-            this.pn_Funktion.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pn_Funktion.Location = new System.Drawing.Point(0, 0);
-            this.pn_Funktion.Name = "pn_Funktion";
-            this.pn_Funktion.Size = new System.Drawing.Size(200, 1050);
-            this.pn_Funktion.TabIndex = 0;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(377, 158);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(537, 231);
-            this.dataGridView1.TabIndex = 1;
-            // 
-            // Verkauf
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(22F, 42F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1924, 1050);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.pn_Funktion);
-            this.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
-            this.Name = "Verkauf";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Verkauf";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.ResumeLayout(false);
-
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel pn_Funktion;
-        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
