c8d95e5a51d93ea49c485a8dc473d2ad59f0d77c
Jacko04 <Kosterschristian@gmail.com>
2021-11-26 13:07:19 +0100

    Anmelde Formular Graphisch

--- Instant-Gaming/Anmelde_formular.Designer.cs
+++ Instant-Gaming/Anmelde_formular.Designer.cs
@@ -29,24 +29,103 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lbl_Registrieren = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(330, 36);
+            this.label1.Font = new System.Drawing.Font("Century", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(324, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(252, 57);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Anmelden";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(329, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(329, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Passwort";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox2.Location = new System.Drawing.Point(469, 131);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox3.Location = new System.Drawing.Point(469, 171);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // lbl_Registrieren
+            // 
+            this.lbl_Registrieren.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lbl_Registrieren.AutoSize = true;
+            this.lbl_Registrieren.Font = new System.Drawing.Font("Century", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Registrieren.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lbl_Registrieren.Location = new System.Drawing.Point(405, 258);
+            this.lbl_Registrieren.Name = "lbl_Registrieren";
+            this.lbl_Registrieren.Size = new System.Drawing.Size(77, 15);
+            this.lbl_Registrieren.TabIndex = 7;
+            this.lbl_Registrieren.Text = "Registrierien ";
+            this.lbl_Registrieren.Click += new System.EventHandler(this.lbl_Registrieren_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button1.Location = new System.Drawing.Point(334, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(235, 51);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Bestätigen";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Anmelde_formular
             // 
             this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl_Registrieren);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
             this.Name = "Anmelde_formular";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Anmelde Formular";
@@ -59,5 +138,11 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lbl_Registrieren;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Instant-Gaming/Anmelde_formular.cs
+++ Instant-Gaming/Anmelde_formular.cs
@@ -16,5 +16,10 @@ namespace Instant_Gaming
         {
             InitializeComponent();
         }
+
+        private void lbl_Registrieren_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
