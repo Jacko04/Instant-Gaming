4c8baeba0e202fa8c95e3d95189a944c888027ae
Jacko04 <Kosterschristian@gmail.com>
2021-11-29 11:11:13 +0100

    Anmelde Formular Bearbeitet

--- Instant-Gaming/Anmelde_formular.Designer.cs
+++ Instant-Gaming/Anmelde_formular.Designer.cs
@@ -31,10 +31,10 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txt_ID = new System.Windows.Forms.TextBox();
+            this.txt_Passwort = new System.Windows.Forms.TextBox();
             this.lbl_Registrieren = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_Bestätigen = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -70,21 +70,21 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "Passwort";
             // 
-            // textBox2
+            // txt_ID
             // 
-            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.textBox2.Location = new System.Drawing.Point(469, 131);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 5;
+            this.txt_ID.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txt_ID.Location = new System.Drawing.Point(469, 131);
+            this.txt_ID.Name = "txt_ID";
+            this.txt_ID.Size = new System.Drawing.Size(100, 20);
+            this.txt_ID.TabIndex = 5;
             // 
-            // textBox3
+            // txt_Passwort
             // 
-            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.textBox3.Location = new System.Drawing.Point(469, 171);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 6;
+            this.txt_Passwort.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txt_Passwort.Location = new System.Drawing.Point(469, 171);
+            this.txt_Passwort.Name = "txt_Passwort";
+            this.txt_Passwort.Size = new System.Drawing.Size(100, 20);
+            this.txt_Passwort.TabIndex = 6;
             // 
             // lbl_Registrieren
             // 
@@ -99,19 +99,20 @@
             this.lbl_Registrieren.Text = "Registrierien ";
             this.lbl_Registrieren.Click += new System.EventHandler(this.lbl_Registrieren_Click);
             // 
-            // button1
+            // btn_Bestätigen
             // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.button1.Location = new System.Drawing.Point(334, 204);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(235, 51);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Bestätigen";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_Bestätigen.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btn_Bestätigen.FlatAppearance.BorderSize = 0;
+            this.btn_Bestätigen.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Bestätigen.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_Bestätigen.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.btn_Bestätigen.Location = new System.Drawing.Point(334, 204);
+            this.btn_Bestätigen.Name = "btn_Bestätigen";
+            this.btn_Bestätigen.Size = new System.Drawing.Size(235, 51);
+            this.btn_Bestätigen.TabIndex = 8;
+            this.btn_Bestätigen.Text = "Bestätigen";
+            this.btn_Bestätigen.UseVisualStyleBackColor = true;
+            this.btn_Bestätigen.Click += new System.EventHandler(this.btn_Bestätigen_Click);
             // 
             // Anmelde_formular
             // 
@@ -119,17 +120,16 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(884, 461);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Bestätigen);
             this.Controls.Add(this.lbl_Registrieren);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txt_Passwort);
+            this.Controls.Add(this.txt_ID);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Name = "Anmelde_formular";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Anmelde Formular";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,9 +140,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txt_ID;
+        private System.Windows.Forms.TextBox txt_Passwort;
         private System.Windows.Forms.Label lbl_Registrieren;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_Bestätigen;
     }
 }
--- Instant-Gaming/Anmelde_formular.cs
+++ Instant-Gaming/Anmelde_formular.cs
@@ -7,19 +7,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace Instant_Gaming
 {
     public partial class Anmelde_formular : Form
     {
+
+        //Variablen
+        OleDbConnection con = new OleDbConnection();
+        OleDbCommand cmd = new OleDbCommand();
+        OleDbDataReader reader;
+
+        Int64 ID;
+        string Passwort;
+
+        
+
         public Anmelde_formular()
         {
             InitializeComponent();
+            
         }
 
         private void lbl_Registrieren_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btn_Bestätigen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        //Methoden
+        //Methode für Anmelden#
+        public void Überprüfen()
+        {   //for schleife zu überprüfung der 2 Tabellen
+            for (int i = 0; i < 2; i++)
+            {
+                //Array zum Speichern der Tabellen
+                string[] Tabellen = {"Mitarbeiter", "Kunden" };
+                //Array zum Definieren der Abfrage
+                string[] Kennung = { "MiD", "PiD" };
+                //Array If Bedingugen
+                int[] Zahlen = { 6, 8 };
+                
+                //Converten der Infromation aus der Form
+                ID = Convert.ToInt64(txt_ID.Text);
+                Passwort = Convert.ToString(txt_Passwort.Text);
+
+                //Datenbankverbindung und Abfrage
+                con.ConnectionString = "Provider = Microsoft.Jet.OLEDB.4.0;" + "Data Source = Instant Gaming Verkauf.mdb ";
+                cmd.Connection = con;
+                cmd.CommandText = "SELECT * FROM " + Tabellen[i] + " WHERE" + Kennung[i] +  "= '" + ID + "' AND passwort = '" + Passwort + "'";
+                con.Open();
+
+                try
+                {
+                    while (reader.Read())
+                    {
+                        if (ID == reader.GetInt64(0) )
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+            }
         }
     }
 }
