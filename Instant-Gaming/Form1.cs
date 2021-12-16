a336dc054acd1df848e94064808b44792c6d50e5
Spawnpoint31 <erikbe10@yahoo.de>
2021-12-16 14:30:24 +0100

    ecd

--- Instant-Gaming/Form1.cs
+++ Instant-Gaming/Form1.cs
@@ -19,30 +19,30 @@ namespace Instant_Gaming
         public Form1()
         {
             InitializeComponent();
-            testfunktion();
+            //testfunktion();
         }
-        private void testfunktion()
-        {
-            int ID;
-            string Email = "Jkonietzny@mymail.none";
-            string Passwort = "1";
+        //private void testfunktion()
+        //{
+        //    int ID;
+        //    string Email = "Jkonietzny@mymail.none";
+        //    string Passwort = "1";
 
-            con.ConnectionString = "Provider = Microsoft.Jet.OLEDB.4.0;" + "Data Source = Instant Gaming Verkauf.mdb ";
-            cmd.Connection = con;
-            //cmd.CommandText = "Select KiD from Kunden where 'E-Mail' = '" + Email + "' and passwort = '" + Passwort + "'";
-            cmd.CommandText = "select * from Kunden where E-Mail = '" + Email + "'" ;
-            con.Open();
-            reader = cmd.ExecuteReader();
+        //    con.ConnectionString = "Provider = Microsoft.Jet.OLEDB.4.0;" + "Data Source = Instant Gaming Verkauf.mdb ";
+        //    cmd.Connection = con;
+        //    //cmd.CommandText = "Select KiD from Kunden where 'E-Mail' = '" + Email + "' and passwort = '" + Passwort + "'";
+        //    cmd.CommandText = "select * from Kunden where E-Mail = '" + Email + "'" ;
+        //    con.Open();
+        //    reader = cmd.ExecuteReader();
 
 
-            while (reader.Read())
-            {
-                ID = reader.GetInt32(0);
-                MessageBox.Show("Ihre ID ist = " + ID);
-            }
-            reader.Close();
+        //    while (reader.Read())
+        //    {
+        //        ID = reader.GetInt32(0);
+        //        MessageBox.Show("Ihre ID ist = " + ID);
+        //    }
+        //    reader.Close();
 
-            con.Close();
-        }
+        //    con.Close();
+        //}
     }
 }
--- Instant-Gaming/Rechnung.cs
+++ Instant-Gaming/Rechnung.cs
@@ -39,7 +39,7 @@ namespace Instant_Gaming
         private void Rechnung_Load(object sender, EventArgs e)
         {
             int Anzahleinträge = 0;
-            sql = "select COUNT(RiD) from Rechnung";
+            sql = "select MAX(RiD) from Rechnung";
             Verbinden(sql);
             reader = cmd.ExecuteReader();
             while (reader.Read())
@@ -48,29 +48,29 @@ namespace Instant_Gaming
             }
             con.Close();
             reader.Close();
-
-            sql = "select DISTINCT RiD from Rechnung where KiD =" + Kid;
+            sql = "select DISTINCT RiD from Rechnung where KiD = " + Kid;
             Verbinden(sql);
             reader = cmd.ExecuteReader();
             while (reader.Read())
             {
-                Ridload.Add(reader.GetInt32(0));
+                dgv_Rechnungen.Rows.Add(reader.GetInt32(0));
+                //Ridload.Add(reader.GetInt32(0));
             }
             con.Close();
             reader.Close();
 
-            for (int i = 0; i < Anzahleinträge; i++)
-            {
-                sql = "select DISTINCT Datum from Rechnung where KiD =" + Kid;
-                Verbinden(sql);
-                reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    dgv_Rechnungen.Rows.Add(Ridload[i], reader.GetDateTime(0));
-                }
-                con.Close();
-                reader.Close();
-            }
+            //for (int i = 0; i < Anzahleinträge; i++)
+            //{
+            //    sql = "select DESTINCT CONVERT( date, GETDATE() ) from Rechnung where KiD =" + Kid;
+            //    Verbinden(sql);
+            //    reader = cmd.ExecuteReader();
+            //    while (reader.Read())
+            //    {
+            //        dgv_Rechnungen.Rows.Add(Ridload[i], reader.GetDateTime(0));
+            //    }
+            //    con.Close();
+            //    reader.Close();
+            //}
         }
 
         public void Verbinden(string sql)
