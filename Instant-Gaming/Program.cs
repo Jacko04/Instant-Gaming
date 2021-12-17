eb9b4dce4a6619bb41e6ac1eaef65a538bea04ac
Jacko04 <Kosterschristian@gmail.com>
2021-12-17 08:45:38 +0100

    OKay lets go

--- Instant-Gaming/Program.cs
+++ Instant-Gaming/Program.cs
@@ -16,7 +16,7 @@ namespace Instant_Gaming
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Anmelde_formular());
         }
     }
 }
